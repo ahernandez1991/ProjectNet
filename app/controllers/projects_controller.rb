d740dd641c77ec9b84d1caef5f86c199624a34c8
Alvaro Hernandez <alvaroh1991@gmail.com>
2018-10-30 09:18:42 -0500

    Vistas de ppal, index, y nuevo proyecto básico

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,14 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @p=Project.new
+  end
+
+  def create
+    @p=Project.new(project_params)
+    if @p.save
+      redirect_to projects_path, notice: "Se ha creado el proyecto exitosamente"
+    end
   end
 
   def edit
@@ -16,4 +24,10 @@ class ProjectsController < ApplicationController
 
   def destroy
   end
+
+private
+  def project_params
+    params.require(:project).permit(:nombre, :fecha)
+  end
+
 end
